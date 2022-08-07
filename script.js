be3a1e11ab6ecc426b9c2fb6b545bd9f6ee40533
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-07 22:45:09 +0530

    new function and choice buttons

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
 
 console.log("hello world");
 
-function getComputerChoice(min ,max){
-    return Math.floor(Math.random() * (max - min)) + min;
+let computerScore = 0;
+let myScore = 0;
+
+function getComputerChoice(){
+    let choice = ["rock","paper","scissor"];
+    let randon_choice = Math.floor(Math.random() * choice.length);
+    return choice[randon_choice];
 }
 
-computerChoice = getComputerChoice(1 , 4);
 
-console.log(getComputerChoice(1 , 4));
+console.log( getComputerChoice());
 
-let mychoice = prompt("rock = 1 paper =2 scissors = 3 :",)
+let computerChoice = getComputerChoice();
 
-if (mychoice == computerChoice){
-    console.log("tie");
+function playGame(mychoice,computerChoice){
+
+    if (mychoice == computerChoice){
+        console.log("tie" );
+    }
+    else if((mychoice == 'rock' && computerChoice == 'scissor') || 
+    (mychoice == 'paper' && computerChoice == 'rock') ||
+    (mychoice == 'scissors' && computerChoice == 'paper')){
+        console.log("you win");
+    }
+    else{
+        console.log("you lose");
+    }
 }
-else{
-    if(mychoice == 1 && computerChoice == 2 ){
-        // me rock and com paper
-        console.log("you lost");
-        alert("you lost");
-    }
-    else if(mychoice == 1 && computerChoice == 3 ){
-        // me rock    comp 3
-        console.log("you win");
-        alert("you win");
-    }
-    else if(mychoice == 2 && computerChoice == 1 ){
-        // me paper   comp rock
-        console.log("you win");
-        alert("you win");
-    }
-    else if(mychoice == 2 && computerChoice == 3 ){
-        // me paper  comp scissors
-        console.log("you lost");
-        alert("you lost");
-    }
-    else if(mychoice == 3 && computerChoice == 1 ){
-        // me scissors  comp rock 
-        console.log("you lost");
-        alert("you lost");
-    }
-    else if(mychoice == 3 || computerChoice == 2 ){
-        // scissors   comp paper
-        console.log("you win");
-        alert("you win");
-    }
 
-}
+
+let mychoice = "rock";
+
+console.log(playGame());
